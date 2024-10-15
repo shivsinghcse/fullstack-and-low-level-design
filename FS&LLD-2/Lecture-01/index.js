8a5657568d056fc4a5cac5826bb3c7b650a24844
Shiv Singh <shivsinghcse@gmail.com>
2024-10-15 13:06:22 +0530

    var, let and const

--- FS&LLD-2/Lecture-01/index.js
+++ FS&LLD-2/Lecture-01/index.js
@@ -1,1 +1,24 @@
-console.log("Hi, I an running from external JS file.");
+console.log('Hi, I an running from external JS file.');
+
+// valid JS
+var a = true;
+console.log(a);
+
+var b = false;
+console.log(b);
+
+// will throw error
+
+// var city = "Bangalore";
+// console.log(city);
+
+// let city = 'Bangalore';
+// console.log(city);
+
+// city = 'Delhi';
+// console.log(city);
+
+// const goat = 'Dhoni';
+// goat = 'Kohli';
+
+// console.log(goat);
