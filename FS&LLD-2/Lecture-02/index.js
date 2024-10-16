a4171f361c39040ce16d95f7d9058797734635d5
Shiv Singh <shivsinghcse@gmail.com>
2024-10-16 12:35:05 +0530

    TDZ

--- FS&LLD-2/Lecture-02/index.js
+++ FS&LLD-2/Lecture-02/index.js
@@ -16,10 +16,41 @@
 
 // Hoisting
 
-console.log(msg);
-greet();
-  
-var msg = 'hello';
-function greet() {
-    console.log(msg, 'inside greet');
-}
+// console.log(msg);
+// greet();
+
+// var msg = 'hello';
+// function greet() {
+//     console.log(msg, 'inside greet');
+// }
+
+// let someVal = 'hello world';
+// var msg = 'hello';
+// function greet() {
+//     var msg = 'i m msg inside greet.'
+//     console.log(msg, 'happy holidays');
+// }
+
+// console.log(msg);
+// greet();
+
+// var msg = 'hello';
+
+// greet1();
+// greet2();
+
+// console.log(msg);
+// function greet1() {
+//     var msg = 'inside greet1';
+//     console.log(msg);
+// }
+
+// function greet2() {
+//     var msg = 'inside greet2';
+//     console.log(msg);
+// }
+
+debugger;
+console.log(a);
+var a = 9; // -- will work
+let a = 9; // error (TDZ)
